15c904791e04e23f1a9838e1a964c2505474960e
dee <dee@adadevelopersacademy.org>
2018-12-07 12:02:50 -0800

    Updates

---
+++ precache-manifest.e4f18e16729722aee48bb3c227e3b6b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c40684fa858b8f943a1",
+    "url": "/exquisite-react/static/js/runtime~main.5c40684f.js"
+  },
+  {
+    "revision": "486919cd172a1bccd99c",
+    "url": "/exquisite-react/static/js/main.486919cd.chunk.js"
+  },
+  {
+    "revision": "2818dbc80ba3c939eefa",
+    "url": "/exquisite-react/static/js/1.2818dbc8.chunk.js"
+  },
+  {
+    "revision": "486919cd172a1bccd99c",
+    "url": "/exquisite-react/static/css/main.1d40b3e9.chunk.css"
+  },
+  {
+    "revision": "127c60836bf8346cc3c110f98e2e49f2",
+    "url": "/exquisite-react/index.html"
+  }
+];
