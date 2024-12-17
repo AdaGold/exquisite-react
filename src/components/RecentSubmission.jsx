95c4606f7c1513425b919f34c84b6198168d6d82
Ansel Rognlie <ansel@adadevelopersacademy.org>
2024-12-16 16:18:09 -0800

    addresses linting issues
    Note that several "no-unused-vars" errors remain since the props and useState calls will be used during the implementation.

--- src/components/RecentSubmission.jsx
+++ src/components/RecentSubmission.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 import './RecentSubmission.css';
 
@@ -6,10 +5,10 @@ const RecentSubmission = (props) => {
   return (
     <div className="RecentSubmission">
       <h3>The Most Recent Submission</h3>
-      <p className="RecentSubmission__submission">{  }</p>
+      <p className="RecentSubmission__submission">{ }</p>
     </div>
   );
-}
+};
 
 RecentSubmission.propTypes = {
   submission: PropTypes.string.isRequired,
